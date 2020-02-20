82c92289ef9ce27ddc62e698619ba65e91baf482
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-02-20 20:56:18 +0100

    increase access token time

--- expense/expense/settings.py
+++ expense/expense/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'expense.wsgi.application'
 DATABASES = DB.config(DEBUG)
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
